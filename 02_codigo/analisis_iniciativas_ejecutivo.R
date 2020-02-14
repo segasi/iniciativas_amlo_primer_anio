884f0709f97f776a35127afa036548979c1ae974
Sebastian Garrido <segasi@gmail.com>
2020-02-14 13:06:41 -0600

    Gráfica del # de iniciativas presentadas por c/presidente en los primeros 14 meses de gobierno

--- 02_codigo/analisis_iniciativas_ejecutivo.R
+++ 02_codigo/analisis_iniciativas_ejecutivo.R
@@ -53,3 +53,44 @@ bd_iniciativas <-
 bd_iniciativas <- 
   bd_iniciativas %>% 
   mutate(subclasificacion = fct_relevel(subclasificacion, "Ley Secundaria", "Reforma Constitucional"))
+
+
+### Gráfica del número de iniciativas presentadas por cada presidente en los primeros 14 meses de gobierno ----
+
+# Datos
+bd_iniciativas %>% 
+  group_by(presidente_acro, subclasificacion) %>% 
+  summarise(numero = n()) %>% 
+  ungroup() %>% 
+  group_by(presidente_acro) %>% 
+  mutate(total = sum(numero)) %>% 
+  ungroup()
+
+# Gráfica
+bd_iniciativas %>% 
+  group_by(presidente_acro, subclasificacion) %>% 
+  summarise(numero = n()) %>% 
+  ungroup() %>% 
+  group_by(presidente_acro) %>% 
+  mutate(total = sum(numero),
+         presidente_acro_grafica = str_c(presidente_acro, "\n (", total, ")")) %>% 
+  ungroup() %>%
+  mutate(presidente_acro_grafica = fct_relevel(presidente_acro_grafica, "Fox\n (42)", "Calderón\n (29)", "Peña Nieto\n (41)", "López Obrador\n (16)")) %>% 
+  ggplot(aes(x = presidente_acro_grafica, y = numero, fill = subclasificacion)) +
+  geom_col() +
+  geom_hline(yintercept = seq(5, 40, 5), color = "white", linetype = 3) +
+  
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_continuous(breaks = seq(0, 40, 5), limits = c(-1, 42), expand = c(0, 0)) +
+  scale_fill_manual(values = c("steelblue", "salmon")) +
+  labs(title = str_wrap("Número de iniciativas de reforma constitucional y a leyes secundarias presentadas por los últimos cuatro presidentes de México en los primeros 14 meses de su gobierno", width = 70),
+       x = NULL,
+       y = "Número\n",
+       caption = "\n   @segasi / Fuente: Sistema de Información Legislativa de SEGOB.\n\n   Datos al 31 de enero de 2002, 2008, 2014 y 2020, respectivamente.",
+       fill = NULL) +
+  tema +
+  theme(panel.grid = element_blank(),
+        legend.position = c(0.885, 0.9)) +
+  ggsave("03_graficas/numero_iniciativas_primeros_14_meses_por_tipo.png", width = 14, height = 9, dpi = 200)
+
+
