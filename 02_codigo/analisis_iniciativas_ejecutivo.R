eabe5cf0ceba25146ee39749c4e400d3394e7c83
Sebastian Garrido <segasi@gmail.com>
2020-02-14 17:13:36 -0600

    Gráfica del estatus de las iniciativas de reforma presentadas por los últimos cuatro presidentes

--- 02_codigo/analisis_iniciativas_ejecutivo.R
+++ 02_codigo/analisis_iniciativas_ejecutivo.R
@@ -93,4 +93,64 @@ bd_iniciativas %>%
         legend.position = c(0.885, 0.9)) +
   ggsave("03_graficas/numero_iniciativas_primeros_14_meses_por_tipo.png", width = 14, height = 9, dpi = 200)
 
+### Gráfica del estatus de las iniciativas de reforma presentadas por los últimos cuatro presidentes al final de los primeros 14 meses de su gobierno ----
+g_numero <- 
+  bd %>% 
+  count(presidente_acro, estatus_al_mes_14, subclasificacion) %>% 
+  group_by(presidente_acro, subclasificacion) %>% 
+  mutate(total = sum(n),
+         porcentaje = (n/total)*100) %>% 
+  ungroup() %>% 
+  ggplot(aes(x= presidente_acro, y = n, fill = estatus_al_mes_14)) +
+  geom_col() +
+  geom_hline(yintercept = seq(10, 40, 10), linetype = 3, color = "white") +
+  facet_wrap(~ subclasificacion) +
+  # scale_y_continuous(expand = c(0, 0), limits = c(-1, 105), breaks = seq(0, 100, 10)) +
+  scale_fill_manual(values = c("#1a9850", "#d9ef8b", "#d73027", "grey90")) +
+  labs(title = str_wrap("Estatus de las iniciativas de reforma a leyes secundarias y a la Constitución presentadas por los últimos cuatro presidentes de México, al final de los primeros 14 meses de su gobierno", width = 70),
+       x = NULL,
+       y = "Número\n",
+       caption = NULL,
+       fill = NULL) +
+  tema +
+  theme(panel.grid = element_blank(),
+        axis.title.y = element_text(angle = 90),
+        strip.background = element_rect(fill = "grey60", color = "grey60"),
+        strip.text = element_text(size = 20, color = "white"), 
+        legend.position = "none",
+        # legend.position = c(0.20, 0.85)
+  ) 
+
+
+g_porcentaje <- 
+  bd %>% 
+  count(presidente_acro, estatus_al_mes_14, subclasificacion) %>% 
+  group_by(presidente_acro, subclasificacion) %>% 
+  mutate(total = sum(n),
+         porcentaje = (n/total)*100) %>% 
+  ungroup() %>% 
+  ggplot(aes(x= presidente_acro, y = porcentaje, fill = estatus_al_mes_14)) +
+  geom_col() +
+  geom_hline(yintercept = seq(20, 80, 20), linetype = 3, color = "white") +
+  facet_wrap(~ subclasificacion) +
+  scale_y_continuous(expand = c(0, 0), limits = c(-1, 105), breaks = seq(0, 100, 20)) +
+  scale_fill_manual(values = c("#1a9850", "#d9ef8b", "#d73027", "grey90")) +
+  labs(title = NULL,
+       x = NULL,
+       y = "Porcentaje\n",
+       caption = "\n   @segasi / Fuente: Sistema de Información Legislativa de SEGOB.\n\n   Datos al 31 de enero de 2002, 2008, 2014 y 2020, respectivamente.",
+       fill = NULL) +
+  tema +
+  theme(panel.grid = element_blank(),
+        axis.title.y = element_text(angle = 90),
+        strip.background = element_rect(fill = "grey60", color = "grey60"),
+        strip.text = element_text(size = 20, color = "white"), 
+        legend.position = "bottom",
+        # legend.position = c(0.20, 0.85)
+  ) 
+
+
+plot_grid(g_numero, g_porcentaje, ncol = 1) + 
+  ggsave("03_graficas/numerp_y_porcentaje_iniciativas_mes_14_por_estatus.png", width = 14, height = 9, dpi = 200)
+
 
