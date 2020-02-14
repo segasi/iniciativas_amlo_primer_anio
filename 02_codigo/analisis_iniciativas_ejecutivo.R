b2bfc566c0b0cc3cfc284c7f60ee9ebebbd4b4f5
Sebastian Garrido <segasi@gmail.com>
2020-02-14 10:19:57 -0600

    Importar datos

--- 02_codigo/analisis_iniciativas_ejecutivo.R
+++ 02_codigo/analisis_iniciativas_ejecutivo.R
@@ -1,2 +1,14 @@
 ### Cargar paquetes, definir setup y tema de gráficas ----
 source("02_codigo/paquetes_setup_tema.R")
+
+### Importar datos ----
+bd_iniciativas <- 
+  read_excel("01_datos/bd_iniciativas_primeros_14_meses.xlsx",
+             col_types = c("text", "text", "text", 
+                           "text", "text", "text", "text", "text", 
+                           "date", "text", "text", "text", "text", 
+                           "text")) %>% 
+  clean_names()
+
+
+# Base de datos construida con información de http://sil.gobernacion.gob.mx/portal
