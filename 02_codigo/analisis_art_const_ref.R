17fcb188d4fbbf347678d9c130b0e66bf0860a18
Sebastian Garrido <segasi@gmail.com>
2020-02-14 10:09:48 -0600

    Importar datos

--- 02_codigo/analisis_art_const_ref.R
+++ 02_codigo/analisis_art_const_ref.R
@@ -1,2 +1,7 @@
 ### Cargar paquetes, definir setup y tema de gráficas ----
 source("02_codigo/paquetes_setup_tema.R")
+
+### Importar datos ----
+art_ref <- 
+  read_excel("01_datos/articulos_constitucion_reformados.xlsx") %>% 
+  mutate(fecha_publicacion = as_date(fecha_publicacion))
